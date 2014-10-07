f39e74a99700a6a652205e4128d91df44e091fac
Martin Vidner <mvidner@suse.cz>
2014-10-07 17:56:15 +0200

    Test the proxy return code test (bnc#853725, bnc#871945).

---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+srcdir = File.expand_path("../../src", __FILE__)
+y2dirs = ENV.fetch("Y2DIR", "").split(":")
+ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
---
+++ test/test_proxy_return_code_test.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env rspec
+require_relative "test_helper"
+require "yast"
+
+
+describe "Yast::ProxyDialogsInclude" do
+  subject do
+    instance = Yast::Module.new
+    Yast.include instance, "proxy/dialogs.rb"
+    instance
+  end
+
+  let(:datadir) { File.expand_path("../data", __FILE__) }
+
+  describe "#TestProxyReturnCode" do
+    it "returns truthy for a good response" do
+      proxy_kind = "HTTP"
+      stderr = File.read(datadir + "/curl-stderr-good")
+
+      expect(subject).to_not receive(:ErrorPopupGeneric)
+      expect(!! subject.TestProxyReturnCode(proxy_kind, stderr)).to eq true
+    end
+
+    it "returns falsey and pops up for a bad response at destination" do
+      proxy_kind = "HTTP"
+      stderr = File.read(datadir + "/curl-stderr-notfound")
+
+      expect(subject).to receive(:ErrorPopupGeneric).with(String, String)
+      expect(!! subject.TestProxyReturnCode(proxy_kind, stderr)).to eq false
+    end
+
+    it "returns falsey and pops up for a bad response at proxy" do
+      proxy_kind = "HTTP"
+      stderr = File.read(datadir + "/curl-stderr-badproxy")
+
+      expect(subject).to receive(:ErrorPopupGeneric).with(String, String)
+      expect(!! subject.TestProxyReturnCode(proxy_kind, stderr)).to eq false
+    end
+  end
+end
