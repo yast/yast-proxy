6dc3292f00f0d06ba60fbfad69456a3c688f6912
Martin Vidner <mvidner@suse.cz>
2015-04-23 16:33:17 +0200

    stole Yast::RSpec::Matchers
    from
    https://github.com/yast/yast-ruby-bindings/blob/b5035d2d2923ae40387568b2a803ab3984d8b0b2/src/ruby/yast/rspec/matchers.rb
    
    (to be reverted when merging to master)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,11 @@ srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
 
-require "yast/rspec"
+require_relative "matchers"
+RSpec.configure do |c|
+  c.include Yast::RSpec::Matchers
+end
+
+def path(s)
+  Yast::Path.new(s)
+end
