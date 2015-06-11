f2cab731cf6ec1b531fe5f0b86607854e09c7245
Martin Vidner <mvidner@suse.cz>
2015-06-11 14:15:54 +0200

    Measure test code coverage.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,22 @@ srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.configure do
+    # Don't measure the tests themselves. We should have named them /spec/.
+    add_filter "/test/"
+  end
+  SimpleCov.start
+
+  # for coverage we need to load all ruby files
+  src_location = File.expand_path("../../src", __FILE__)
+  # note that clients/ are excluded because they run too eagerly by design
+  Dir["#{src_location}/{include,modules}/**/*.rb"].each do |f|
+    require_relative f
+  end
+end
+
 require_relative "matchers"
 RSpec.configure do |c|
   c.include Yast::RSpec::Matchers
