1bfd312743d49c8b9b451423ed67c84d609bb1b9
Josef Reidinger <jreidinger@suse.cz>
2020-06-09 17:45:22 +0200

    unify test and also add test for reduced profile

--- test/proxy_test.rb
+++ test/proxy_test.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env rspec
+
 require_relative "test_helper"
 require "yast"
 
@@ -124,6 +125,67 @@ describe "Yast::ProxyClass" do
         and_return true
 
       expect(subject.WriteCurlrc).to be true
+    end
+  end
+
+  context "common Read-Export test" do
+    before do
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".sysconfig.proxy.HTTP_PROXY")).
+        and_return "h"
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".sysconfig.proxy.HTTPS_PROXY")).
+        and_return "hs"
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".sysconfig.proxy.FTP_PROXY")).
+        and_return "f"
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".sysconfig.proxy.NO_PROXY")).
+        and_return nil
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".sysconfig.proxy.PROXY_ENABLED")).
+        and_return "yes"
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".target.size"), "/root/.curlrc").
+        and_return 42
+
+      allow(Yast::SCR).to receive(:Read).
+        with(path(".root.curlrc.\"--proxy-user\"")).
+       and_return "user:pass"
+    end
+
+    describe "#Read" do
+      it "reads /etc/sysconfig/proxy and /root/.curlrc via SCR" do
+        expect(subject.Read).to be true
+      end
+    end
+
+    describe "#Export" do
+      it "returns an appropriate hash" do
+        subject.Read
+        expect(subject.Export).to eq({
+          "enabled"        => true,
+          "ftp_proxy"      => "f",
+          "http_proxy"     => "h",
+          "https_proxy"    => "hs",
+          "no_proxy"       => "",
+          "proxy_password" => "pass",
+          "proxy_user"     => "user"
+        })
+      end
+
+      it "returns limited hash when proxy is disabled" do
+        subject.enabled = false
+
+        expect(subject.Export).to eq({
+          "enabled" => false
+        })
+      end
     end
   end
 end
