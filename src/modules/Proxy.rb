7dbcb6a913cd2887b70b5a6eefd584b60ff1b16f
Knut Anderssen <kanderssen@suse.com>
2021-06-09 10:41:58 +0100

    Relax rubocop Class/Length by now

--- src/modules/Proxy.rb
+++ src/modules/Proxy.rb
@@ -5,7 +5,6 @@ module Yast
   # Configures FTP and HTTP proxies via sysconfig
   # and /root/.curlrc (for YOU)
   class ProxyClass < Module
-
     # @return [Boolan] Whether the configuration should be copied to the target system
     attr_accessor :to_target
 
